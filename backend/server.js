de44566dd137ede3393d577ca416e14a478efd30
dngoyb <dngoyb@gmail.com>
2021-05-04 17:37:07 +0200

    front and backend connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
+dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -13,7 +15,11 @@ app.get('/api/products', (req, res) => {
 
 app.get('/api/products/:id', (req, res) => {
 	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
+	res.json(products);
 });
 
-app.listen(5000, console.log('server running'));
+const PORT = process.env.PORT || 5000;
+app.listen(
+	PORT,
+	console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
